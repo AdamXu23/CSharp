039afbdbecddc33e1c31bf4370467c5f2639c57f
AdamXu23 <xsz1993@live.com>
2021-08-04 14:24:41 +0800

    20210804-1424

--- ADO_SQLExoress_TEST/ADO_SQLExoress_TEST/Form1.Designer.cs
+++ ADO_SQLExoress_TEST/ADO_SQLExoress_TEST/Form1.Designer.cs
@@ -30,7 +30,7 @@ namespace ADO_SQLExoress_TEST
         private void InitializeComponent()
         {
             this.Open_button = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SQL_LOG = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Open_button
@@ -44,22 +44,22 @@ namespace ADO_SQLExoress_TEST
             this.Open_button.UseVisualStyleBackColor = true;
             this.Open_button.Click += new System.EventHandler(this.Open_button_Click);
             // 
-            // textBox1
+            // SQL_LOG
             // 
-            this.textBox1.Font = new System.Drawing.Font("SimSun", 20.25F, System.Drawing.FontStyle.Bold);
-            this.textBox1.Location = new System.Drawing.Point(12, 57);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(776, 381);
-            this.textBox1.TabIndex = 1;
+            this.SQL_LOG.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SQL_LOG.Location = new System.Drawing.Point(12, 57);
+            this.SQL_LOG.Multiline = true;
+            this.SQL_LOG.Name = "SQL_LOG";
+            this.SQL_LOG.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SQL_LOG.Size = new System.Drawing.Size(1018, 381);
+            this.SQL_LOG.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1042, 450);
+            this.Controls.Add(this.SQL_LOG);
             this.Controls.Add(this.Open_button);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -72,7 +72,7 @@ namespace ADO_SQLExoress_TEST
         #endregion
 
         private System.Windows.Forms.Button Open_button;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox SQL_LOG;
     }
 }
 
--- ADO_SQLExoress_TEST/ADO_SQLExoress_TEST/Form1.cs
+++ ADO_SQLExoress_TEST/ADO_SQLExoress_TEST/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime;//引用運行環境時間
 using System.Data;//引用ADO.NET基礎物件
 //using System.Data.OleDb;//引用OLE DB資料來源物件
 using System.Data.SqlClient;//引用SQL Server 資料來源物件
@@ -21,14 +22,43 @@ namespace ADO_SQLExoress_TEST
         }
         //建立SqlConnection物件
         SqlConnection cn = new SqlConnection();
+
+        //顯示連綫參數
+        private void ShowConnection()
+        {
+            SQL_LOG.AppendText( DateTime.Now.ToString() + Environment.NewLine);
+            SQL_LOG.AppendText("ConnectionString\t:" + cn.ConnectionString + Environment.NewLine);
+            SQL_LOG.AppendText("ConnectionTimeout\t:" + cn.ConnectionTimeout + Environment.NewLine);
+            SQL_LOG.AppendText("Database\t\t:" + cn.Database + Environment.NewLine);
+            SQL_LOG.AppendText("DataSource\t\t:" + cn.DataSource + Environment.NewLine);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            cn.ConnectionString = 
+            //創建鏈接字串
+            cn.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\Administrator\\Documents\\GitHub\\CSharp\\ADO_SQLExoress_TEST\\ADO_SQLExoress_TEST\\MyDB.mdf; Integrated Security = True";
+            //顯示鏈接參數
+            ShowConnection();
         }
 
         private void Open_button_Click(object sender, EventArgs e)
         {
-
+            //判斷資料路目前的狀態
+            if(cn.State == ConnectionState.Closed)
+            {
+                //資料庫如果沒有連綫
+                cn.Open();
+                Open_button.Text = "Close";
+                SQL_LOG.AppendText(DateTime.Now.ToString() + Environment.NewLine);
+                SQL_LOG.AppendText("Event\t\t:Database is linked." + Environment.NewLine);
+            }
+            else
+            {
+                cn.Close();
+                Open_button.Text = "Open";
+                SQL_LOG.AppendText(DateTime.Now.ToString() + Environment.NewLine);
+                SQL_LOG.AppendText("Event\t\t:Database disconnected." + Environment.NewLine);
+            }
         }
     }
 }
