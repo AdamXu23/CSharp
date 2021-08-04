fc1b315175facdc8badda2f4ab943c5fab7680df
AdamXu23 <xsz1993@live.com>
2021-08-04 11:41:02 +0800

    20210804-1140

---
+++ ADO_SQLExoress_TEST/ADO_SQLExoress_TEST/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;//引用ADO.NET基礎物件
+using System.Data.OleDb;//引用OLE DB資料來源物件
+using System.Data.SqlClient;//引用SQL Server 資料來源物件
+
+namespace ADO_SQLExoress_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
