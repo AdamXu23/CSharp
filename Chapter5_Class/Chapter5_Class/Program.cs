8cf566ea827018aeec66ef5b74cae431b87ca93e
AdamXu23 <xsz1993@live.com>
2021-08-05 16:21:21 +0800

    20210805-1620

--- Chapter5_Class/Chapter5_Class/Program.cs
+++ Chapter5_Class/Chapter5_Class/Program.cs
@@ -10,9 +10,13 @@ namespace Chapter5_Class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Create a Car Class");
-            
+            //建立一個Benz 物件類別為Car
+            Class.Car Benz = new Class.Car();
+            //設定Benz 的速度為100
+            Benz.Speed = 100;
+            Console.WriteLine("Benz.Speed = {0}",Benz.Speed);
             Console.Read();//讓命令提示字窗維持住
         }
+
     }
 }
