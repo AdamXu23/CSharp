b9820a374843a914fc8b994f8d232400bf5af92f
AdamXu23 <xsz1993@live.com>
2021-08-05 15:45:19 +0800

    202108051545

---
+++ Chapter5_Class/Chapter5_Class/Class/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5_Class.Class
+{
+    class Car
+    {
+        public int Speed;//宣告Speed為public共用變數
+    }
+}
