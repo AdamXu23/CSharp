2632d0177b1576390e253f6370fb10f60224de26
AdamXu23 <xsz1993@live.com>
2021-08-05 18:17:49 +0800

    202108051817

--- Chapter5_Class/Chapter5_Class/Program.cs
+++ Chapter5_Class/Chapter5_Class/Program.cs
@@ -18,7 +18,8 @@ namespace Chapter5_Class
             //設定Benz 的速度為500
             Benz.SetSpeed(500);
             Console.WriteLine("Benz._Speed = {0}", Benz.GetSpeed());
-            Console.WriteLine("Benz.Angle = {0}", Benz.Angle);//Angle只能讀不能寫
+            Console.WriteLine("Benz.Angle = {0}", Benz.Angle);//Angle屬性只能讀不能寫
+            Benz.Turbo = true;//Turbo屬性只能寫不能讀
             Console.Read();//讓命令提示字窗維持住
         }
 
