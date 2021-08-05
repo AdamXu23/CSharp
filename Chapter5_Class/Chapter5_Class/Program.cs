1b35928e1277c53479bd32201938cd89fd2dc33a
AdamXu23 <xsz1993@live.com>
2021-08-05 16:15:57 +0800

    202108051615

--- Chapter5_Class/Chapter5_Class/Program.cs
+++ Chapter5_Class/Chapter5_Class/Program.cs
@@ -10,6 +10,9 @@ namespace Chapter5_Class
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Create a Car Class");
+            
+            Console.Read();//讓命令提示字窗維持住
         }
     }
 }
