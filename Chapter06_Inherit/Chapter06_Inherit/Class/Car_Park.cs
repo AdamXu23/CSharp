b481239dcd2445db21736358bbd654d8e9849ea4
AdamXu23 <xsz1993@live.com>
2021-08-06 09:26:24 +0800

    202108060926

--- Chapter06_Inherit/Chapter06_Inherit/Class/Car_Park.cs
+++ Chapter06_Inherit/Chapter06_Inherit/Class/Car_Park.cs
@@ -8,5 +8,15 @@ namespace Chapter06_Inherit.Class
 {
     class Car_Park:Car
     {
+        private DateTime Parking_Time;
+        private DateTime Departure_Time;
+        public void Park(DateTime dt)
+        {
+            Parking_Time = dt;
+        }
+        public void Departure(DateTime dt)
+        {
+            Departure_Time = dt;
+        }
     }
 }
