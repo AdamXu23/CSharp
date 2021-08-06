cf07e6c21d2e8279c2bede1a85f1e5a9061361d7
AdamXu23 <xsz1993@live.com>
2021-08-06 11:25:05 +0800

    202108061125

--- Chapter06_Inherit/Chapter06_Inherit/Class/Car_Park.cs
+++ Chapter06_Inherit/Chapter06_Inherit/Class/Car_Park.cs
@@ -12,6 +12,9 @@ namespace Chapter06_Inherit.Class
         private DateTime _Parking_Time;//加入停車時間屬性
         private DateTime _Departure_Time;//加入離開時間
 
+        public int No { get; set; }//No屬性記錄是第幾部車
+        public static int Total { get; set; }//Total靜態屬性記錄車子總數
+
         public DateTime Parking_Time 
         { 
             get
@@ -32,19 +35,17 @@ namespace Chapter06_Inherit.Class
             string str = "Parking_Time:\t" + _Parking_Time.ToString()+ "\tDeparture_Time:\t"+ _Departure_Time.ToString();
             return str;
         }
-
-        public Car_Park()
-        {
-            _Parking_Time = System.DateTime.Now;
-        }
         public Car_Park(Car vCar)
         {
             Set_Car(vCar);
+            Total++;//總共記錄數量+1
+            No = Total;//記錄是第多少輛；
             _Parking_Time = System.DateTime.Now;
         }
         public override string ToString()
         {
-            return "Name=" + Get_Name() + 
+            return "No = " + No.ToString()+
+                "\tName=" + Get_Name() + 
                 "\tBrands=" + Brands + 
                 "\tTurbo=" +GetTurbo().ToString() + Get_Speed_range()+ 
                 "\tParking_Time="+_Parking_Time.ToString()+ 
--- Chapter06_Inherit/Chapter06_Inherit/Program.cs
+++ Chapter06_Inherit/Chapter06_Inherit/Program.cs
@@ -10,10 +10,10 @@ namespace Chapter06_Inherit
     {
         static void Main(string[] args)
         {
-            Class.Car Ada_Car = new Class.Car("Ada", "Benz", true, 380, 0);
-            Console.WriteLine(Ada_Car.ToString());
-            Class.Car_Park Ada_Car_Park = new Class.Car_Park(Ada_Car);
-            Console.WriteLine(Ada_Car_Park.ToString());
+            Class.Car Ada_Car = new Class.Car("Ada", "Benz", true, 380, 0);//宣告Ada_Car物件為Car類別，填入屬性
+            Console.WriteLine(Ada_Car.ToString());//輸出Ada_Car物件屬性
+            Class.Car_Park Ada_Car_Park = new Class.Car_Park(Ada_Car);//宣告Ada_Car_Park物件為Car_Park類別，以Ada_Car屬性為Car_Park的屬性
+            Console.WriteLine(Ada_Car_Park.ToString());//輸出Ada_Car_Park物件屬性
             Console.Read();
         }
     }
