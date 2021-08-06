7a89556d1338dd42e70191845136e15721b91070
AdamXu23 <xsz1993@live.com>
2021-08-06 11:31:01 +0800

    202108061130

--- Chapter06_Inherit/Chapter06_Inherit/Program.cs
+++ Chapter06_Inherit/Chapter06_Inherit/Program.cs
@@ -12,8 +12,16 @@ namespace Chapter06_Inherit
         {
             Class.Car Ada_Car = new Class.Car("Ada", "Benz", true, 380, 0);//宣告Ada_Car物件為Car類別，填入屬性
             Console.WriteLine(Ada_Car.ToString());//輸出Ada_Car物件屬性
-            Class.Car_Park Ada_Car_Park = new Class.Car_Park(Ada_Car);//宣告Ada_Car_Park物件為Car_Park類別，以Ada_Car屬性為Car_Park的屬性
+            //宣告Ada_Car_Park物件為Car_Park類別，以Ada_Car屬性為Car_Park的屬性
+            //Car_Park Total 自動+1
+            Class.Car_Park Ada_Car_Park = new Class.Car_Park(Ada_Car);
             Console.WriteLine(Ada_Car_Park.ToString());//輸出Ada_Car_Park物件屬性
+            Class.Car Aaliyah_Car = new Class.Car("Aaliyah", "Audi", true, 280, 0);//宣告Aaliyah_Car物件為Car類別，填入屬性
+            Console.WriteLine(Aaliyah_Car.ToString());//輸出Aaliyah_Car物件屬性
+            //宣告Aaliyah_Car_Park物件為Car_Park類別，以Aaliyah_Car屬性為Car_Park的屬性
+            //Car_Park Total 自動+1
+            Class.Car_Park Aaliyah_Car_Park = new Class.Car_Park(Aaliyah_Car);
+            Console.WriteLine(Aaliyah_Car_Park.ToString());//輸出Ada_Car_Park物件屬性
             Console.Read();
         }
     }
