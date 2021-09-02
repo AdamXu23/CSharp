887c86870fd209453fa7cff99e331862b12f0427
AdamXu <xsz1993@live.com>
2021-09-02 10:39:33 +0800

    202109021039

--- XmlDocument_TEST/XmlDocument_TEST/Class/Parameters_xml.cs
+++ XmlDocument_TEST/XmlDocument_TEST/Class/Parameters_xml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.IO;
+using Brooks.WinSECS;
 
 namespace XmlDocument_TEST
 {
@@ -12,26 +13,14 @@ namespace XmlDocument_TEST
     {
         private string xmlName = "Parameters.xml";
         private XmlDocument document = new XmlDocument() ;
-        private XmlNode Parameters ;
-        private XmlNode SECS ;
-        private XmlNode General ;
-        public enum SECS_PORT_TYPE
-        {
-            HSMS = 1,
-            SECS1_SERIAL = 2,
-            SECS1_TCPIP = 3,
-            SECS1_TELNET = 4
-        }
-
-        public enum HSMS_CONNECTION_MODE
-        {
-            ACTIVE = 0,
-            PASSIVE = 1,
-            ALTERNATING = 2
-        }
+        private XmlNode Parameters_NODE ;
+        private XmlNode SECS_Node ;
+        private XmlNode General_Node;
+        private XmlNode HSMS_Node;
 
         public Parameters_xml(string _xmlName)
         {
+            document = new XmlDocument();
             xmlName = _xmlName;
             bool result = File.Exists(xmlName);
             if (result == true)
@@ -43,109 +32,319 @@ namespace XmlDocument_TEST
                 XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "UTF-8", "");//xml文件的宣告部分
                 document.AppendChild(declaration);
 
-                Parameters = document.CreateElement("Parameters");
-                document.AppendChild(Parameters);
+                Parameters_NODE = document.CreateElement("Parameters");
+                document.AppendChild(Parameters_NODE);
 
-                SECS = document.CreateElement("SECS");
-                Parameters.AppendChild(SECS);//新增到 Parameters 節點中
+                SECS_Node = document.CreateElement("SECS");
+                Parameters_NODE.AppendChild(SECS_Node);//新增到 Parameters 節點中
 
-                XmlElement HSMS = document.CreateElement("HSMS");
-                SECS.AppendChild(HSMS);//新增到 SECS 節點中
+                HSMS_Node = document.CreateElement("HSMS");
+                SECS_Node.AppendChild(HSMS_Node);//新增到 SECS 節點中
 
-                General = document.CreateElement("General");
-                SECS.AppendChild(General);//新增到 SECS 節點中
+                General_Node = document.CreateElement("General");
+                SECS_Node.AppendChild(General_Node);//新增到 SECS 節點中
 
                 XmlElement AutoDevice = document.CreateElement("AutoDevice");
                 AutoDevice.InnerText = true.ToString();
-                General.AppendChild(AutoDevice);//新增到 SECS 節點中
+                General_Node.AppendChild(AutoDevice);//新增到 SECS 節點中
 
                 XmlElement SECS_MonitorEnabled = document.CreateElement("MonitorEnabled");
                 SECS_MonitorEnabled.InnerText = false.ToString();
-                General.AppendChild(SECS_MonitorEnabled);//新增到 SECS 節點中
+                General_Node.AppendChild(SECS_MonitorEnabled);//新增到 SECS 節點中
 
                 XmlElement SECS_MultipleOpen = document.CreateElement("MultipleOpen");
                 SECS_MultipleOpen.InnerText = true.ToString();
-                General.AppendChild(SECS_MultipleOpen);//新增到 SECS 節點中
+                General_Node.AppendChild(SECS_MultipleOpen);//新增到 SECS 節點中
 
                 XmlElement SECS_PortType = document.CreateElement("PortType");
                 SECS_PortType.InnerText = SECS_PORT_TYPE.HSMS.ToString();
-                General.AppendChild(SECS_PortType);//新增到 SECS 節點中
+                General_Node.AppendChild(SECS_PortType);//新增到 SECS 節點中
 
                 XmlElement SECS_DefaultDeviceID = document.CreateElement("DefaultDeviceID");
                 long DefaultDeviceID = 0;
                 SECS_DefaultDeviceID.InnerText = DefaultDeviceID.ToString();
-                General.AppendChild(SECS_DefaultDeviceID);//新增到 SECS 節點中
+                General_Node.AppendChild(SECS_DefaultDeviceID);//新增到 SECS 節點中
 
                 XmlElement SECS_T3 = document.CreateElement("T3");
                 uint T = 45;
                 SECS_T3.InnerText = T.ToString();
-                HSMS.AppendChild(SECS_T3);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_T3);//新增到 SECS 節點中
 
                 XmlElement SECS_T5 = document.CreateElement("T5");
                 T = 10;
                 SECS_T5.InnerText = T.ToString();
-                HSMS.AppendChild(SECS_T5);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_T5);//新增到 SECS 節點中
 
                 XmlElement SECS_T6 = document.CreateElement("T6");
                 T = 5;
                 SECS_T6.InnerText = T.ToString();
-                HSMS.AppendChild(SECS_T6);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_T6);//新增到 SECS 節點中
 
                 XmlElement SECS_T7 = document.CreateElement("T7");
                 T = 10;
                 SECS_T7.InnerText = T.ToString();
-                HSMS.AppendChild(SECS_T7);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_T7);//新增到 SECS 節點中
 
                 XmlElement SECS_T8 = document.CreateElement("T8");
                 T = 5;
                 SECS_T8.InnerText = T.ToString();
-                HSMS.AppendChild(SECS_T8);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_T8);//新增到 SECS 節點中
 
                 XmlElement SECS_ConnectionMode = document.CreateElement("ConnectionMode");
                 SECS_ConnectionMode.InnerText = HSMS_CONNECTION_MODE.PASSIVE.ToString();
-                HSMS.AppendChild(SECS_ConnectionMode);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_ConnectionMode);//新增到 SECS 節點中
 
                 XmlElement SECS_LocalIPAddress = document.CreateElement("LocalIPAddress");
                 SECS_LocalIPAddress.InnerText = "127.0.0.1";
-                HSMS.AppendChild(SECS_LocalIPAddress);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_LocalIPAddress);//新增到 SECS 節點中
 
                 XmlElement SECS_RemoteIPAddress = document.CreateElement("RemoteIPAddress");
                 SECS_RemoteIPAddress.InnerText = "127.0.0.1";
-                HSMS.AppendChild(SECS_RemoteIPAddress);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_RemoteIPAddress);//新增到 SECS 節點中
 
                 XmlElement SECS_LocalIPPort = document.CreateElement("LocalIPPort");
                 uint port = 5100;
                 SECS_LocalIPPort.InnerText = port.ToString();
-                HSMS.AppendChild(SECS_LocalIPPort);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_LocalIPPort);//新增到 SECS 節點中
 
                 XmlElement SECS_RemoteIPPort = document.CreateElement("RemoteIPPort");
                 SECS_RemoteIPPort.InnerText = port.ToString();
-                HSMS.AppendChild(SECS_RemoteIPPort);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_RemoteIPPort);//新增到 SECS 節點中
 
                 XmlElement SECS_LinkTestTimer = document.CreateElement("LinkTestTimer");
                 long Slt = 60;
                 SECS_LinkTestTimer.InnerText = Slt.ToString();
-                HSMS.AppendChild(SECS_LinkTestTimer);//新增到 SECS 節點中
+                HSMS_Node.AppendChild(SECS_LinkTestTimer);//新增到 SECS 節點中
 
                 document.Save(xmlName);//將生成好的xml儲存到test.xml檔案中
             }
-            XmlNode Parameters = document.SelectSingleNode("Parameters");
-            XmlNode SECS = Parameters.SelectSingleNode("SECS");
-            XmlNode General = SECS.SelectSingleNode("General");
+            Parameters_NODE = document.SelectSingleNode("Parameters");
+            SECS_Node = Parameters_NODE.SelectSingleNode("SECS");
+            General_Node = SECS_Node.SelectSingleNode("General");
+            HSMS_Node = SECS_Node.SelectSingleNode("HSMS");
+
         }
         
         public bool General_AutoDevice
         {
             get 
             {
-                XmlElement AutoDevice = (XmlElement)General.SelectSingleNode("AutoDevice");
+                XmlElement AutoDevice = (XmlElement)General_Node.SelectSingleNode("AutoDevice");
                 return bool.Parse(AutoDevice.InnerText);
             }
             set 
             {
-                XmlElement AutoDevice = (XmlElement)General.SelectSingleNode("AutoDevice");
+                XmlElement AutoDevice = (XmlElement)General_Node.SelectSingleNode("AutoDevice");
                 AutoDevice.InnerText = value.ToString(); 
             }
         }
+        public bool General_MonitorEnabled
+        {
+            get
+            {
+                XmlElement MonitorEnabled = (XmlElement)General_Node.SelectSingleNode("MonitorEnabled");
+                return bool.Parse(MonitorEnabled.InnerText);
+            }
+            set
+            {
+                XmlElement MonitorEnabled = (XmlElement)General_Node.SelectSingleNode("MonitorEnabled");
+                MonitorEnabled.InnerText = value.ToString();
+            }
+        }
+        public bool General_MultipleOpen
+        {
+            get
+            {
+                XmlElement MultipleOpen = (XmlElement)General_Node.SelectSingleNode("MultipleOpen");
+                return bool.Parse(MultipleOpen.InnerText);
+            }
+            set
+            {
+                XmlElement MultipleOpen = (XmlElement)General_Node.SelectSingleNode("MultipleOpen");
+                MultipleOpen.InnerText = value.ToString();
+            }
+        }
+        public SECS_PORT_TYPE General_PortType
+        {
+            get
+            {
+                XmlElement PortType = (XmlElement)General_Node.SelectSingleNode("PortType");
+                
+                return (SECS_PORT_TYPE)Enum.Parse(typeof(SECS_PORT_TYPE), PortType.InnerText);
+            }
+            set
+            {
+                XmlElement PortType = (XmlElement)General_Node.SelectSingleNode("PortType");
+                PortType.InnerText = value.ToString();
+            }
+        }
+        public long General_DefaultDeviceID
+        {
+            get
+            {
+                XmlElement DefaultDeviceID = (XmlElement)General_Node.SelectSingleNode("DefaultDeviceID");
+
+                return long.Parse(DefaultDeviceID.InnerText);
+            }
+            set
+            {
+                XmlElement DefaultDeviceID = (XmlElement)General_Node.SelectSingleNode("DefaultDeviceID");
+                DefaultDeviceID.InnerText = value.ToString();
+            }
+        }
+
+        public long HSMS_T3
+        {
+            get
+            {
+                XmlElement T3 = (XmlElement)HSMS_Node.SelectSingleNode("T3");
+
+                return long.Parse(T3.InnerText);
+            }
+            set
+            {
+                XmlElement T3 = (XmlElement)HSMS_Node.SelectSingleNode("T3");
+                T3.InnerText = value.ToString();
+            }
+        }
+        public long HSMS_T5
+        {
+            get
+            {
+                XmlElement T5 = (XmlElement)HSMS_Node.SelectSingleNode("T5");
+
+                return long.Parse(T5.InnerText);
+            }
+            set
+            {
+                XmlElement T5 = (XmlElement)HSMS_Node.SelectSingleNode("T5");
+                T5.InnerText = value.ToString();
+            }
+        }
+        public long HSMS_T6
+        {
+            get
+            {
+                XmlElement T6 = (XmlElement)HSMS_Node.SelectSingleNode("T6");
+
+                return long.Parse(T6.InnerText);
+            }
+            set
+            {
+                XmlElement T6 = (XmlElement)HSMS_Node.SelectSingleNode("T6");
+                T6.InnerText = value.ToString();
+            }
+        }
+        public long HSMS_T7
+        {
+            get
+            {
+                XmlElement T7 = (XmlElement)HSMS_Node.SelectSingleNode("T7");
+
+                return long.Parse(T7.InnerText);
+            }
+            set
+            {
+                XmlElement T7 = (XmlElement)HSMS_Node.SelectSingleNode("T7");
+                T7.InnerText = value.ToString();
+            }
+        }
+        public long HSMS_T8
+        {
+            get
+            {
+                XmlElement T8 = (XmlElement)HSMS_Node.SelectSingleNode("T8");
+
+                return long.Parse(T8.InnerText);
+            }
+            set
+            {
+                XmlElement T8 = (XmlElement)HSMS_Node.SelectSingleNode("T8");
+                T8.InnerText = value.ToString();
+            }
+        }
+        public HSMS_CONNECTION_MODE HSMS_ConnectionMode
+        {
+            get
+            {
+                XmlElement ConnectionMode = (XmlElement)HSMS_Node.SelectSingleNode("ConnectionMode");
+
+                return (HSMS_CONNECTION_MODE)Enum.Parse(typeof(HSMS_CONNECTION_MODE), ConnectionMode.InnerText);
+            }
+            set
+            {
+                XmlElement ConnectionMode = (XmlElement)HSMS_Node.SelectSingleNode("ConnectionMode");
+                ConnectionMode.InnerText = value.ToString();
+            }
+        }
+        public string HSMS_LocalIPAddress
+        {
+            get
+            {
+                XmlElement LocalIPAddress = (XmlElement)HSMS_Node.SelectSingleNode("LocalIPAddress");
+
+                return LocalIPAddress.InnerText;
+            }
+            set
+            {
+                XmlElement LocalIPAddress = (XmlElement)HSMS_Node.SelectSingleNode("LocalIPAddress");
+                LocalIPAddress.InnerText = value.ToString();
+            }
+        }
+        public string HSMS_RemoteIPAddress
+        {
+            get
+            {
+                XmlElement RemoteIPAddress = (XmlElement)HSMS_Node.SelectSingleNode("RemoteIPAddress");
+
+                return RemoteIPAddress.InnerText;
+            }
+            set
+            {
+                XmlElement RemoteIPAddress = (XmlElement)HSMS_Node.SelectSingleNode("RemoteIPAddress");
+                RemoteIPAddress.InnerText = value.ToString();
+            }
+        }
+        public uint HSMS_LocalIPPort
+        {
+            get
+            {
+                XmlElement LocalIPPort = (XmlElement)HSMS_Node.SelectSingleNode("LocalIPPort");
+
+                return uint.Parse(LocalIPPort.InnerText);
+            }
+            set
+            {
+                XmlElement LocalIPPort = (XmlElement)HSMS_Node.SelectSingleNode("LocalIPPort");
+                LocalIPPort.InnerText = value.ToString();
+            }
+        }
+        public uint HSMS_RemoteIPPort
+        {
+            get
+            {
+                XmlElement RemoteIPPort = (XmlElement)HSMS_Node.SelectSingleNode("RemoteIPPort");
+                return uint.Parse(RemoteIPPort.InnerText);
+            }
+            set
+            {
+                XmlElement RemoteIPPort = (XmlElement)HSMS_Node.SelectSingleNode("RemoteIPPort");
+                RemoteIPPort.InnerText = value.ToString();
+            }
+        }
+        public uint HSMS_LinkTestTimer
+        {
+            get
+            {
+                XmlElement LinkTestTimer = (XmlElement)HSMS_Node.SelectSingleNode("LinkTestTimer");
+                return uint.Parse(LinkTestTimer.InnerText);
+            }
+            set
+            {
+                XmlElement LinkTestTimer = (XmlElement)HSMS_Node.SelectSingleNode("LinkTestTimer");
+                LinkTestTimer.InnerText = value.ToString();
+            }
+        }
+
     }
 }
--- XmlDocument_TEST/XmlDocument_TEST/Form1.cs
+++ XmlDocument_TEST/XmlDocument_TEST/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.IO;
+using Brooks.WinSECS;
 
 namespace XmlDocument_TEST
 {
@@ -18,6 +19,14 @@ namespace XmlDocument_TEST
         public Form1()
         {
             InitializeComponent();
+            foreach (var item in Enum.GetValues(typeof(SECS_PORT_TYPE)))
+            {
+                PortType_CB.Items.Add(item);
+            }
+            foreach (var item in Enum.GetValues(typeof(HSMS_CONNECTION_MODE)))
+            {
+                ConnectionMode_CB.Items.Add(item);
+            }
             Parameters = new Parameters_xml("Parameters.xml");
         }
 
@@ -29,85 +38,39 @@ namespace XmlDocument_TEST
 
         private void General_Load()
         {
-            XmlNode Parameters = document.SelectSingleNode("Parameters");
-            XmlNode SECS = Parameters.SelectSingleNode("SECS");
-            XmlNode General = SECS.SelectSingleNode("General");
+            AutoDevice_CB.Checked = Parameters.General_AutoDevice;
 
-            XmlElement AutoDevice = (XmlElement)General.SelectSingleNode("AutoDevice");
-            AutoDevice_CB.Checked = bool.Parse(AutoDevice.InnerText);
+            MonitorEnabled_CB.Checked = Parameters.General_MultipleOpen;
 
-            XmlElement MonitorEnabled = (XmlElement)General.SelectSingleNode("MonitorEnabled");
-            MonitorEnabled_CB.Checked = bool.Parse(MonitorEnabled.InnerText);
+            MultipleOpen_CB.Checked = Parameters.General_MultipleOpen;
+            
+            PortType_CB.SelectedItem = Parameters.General_PortType;
 
-            XmlElement MultipleOpen = (XmlElement)General.SelectSingleNode("MultipleOpen");
-            MultipleOpen_CB.Checked = bool.Parse(MultipleOpen.InnerText);
-
-            PortType_CB.Items.Add(SECS_PORT_TYPE.HSMS);
-            PortType_CB.Items.Add(SECS_PORT_TYPE.SECS1_SERIAL);
-            PortType_CB.Items.Add(SECS_PORT_TYPE.SECS1_TCPIP);
-            PortType_CB.Items.Add(SECS_PORT_TYPE.SECS1_TELNET);
-            XmlElement PortType = (XmlElement)General.SelectSingleNode("PortType");
-            PortType_CB.SelectedItem = (SECS_PORT_TYPE)Enum.Parse(typeof(SECS_PORT_TYPE), PortType.InnerText);
-
-            XmlElement DefaultDeviceID = (XmlElement)General.SelectSingleNode("DefaultDeviceID");
-            DefaultDeviceID_numericUpDown.Value = long.Parse(DefaultDeviceID.InnerText);
+            DefaultDeviceID_numericUpDown.Value = Parameters.General_DefaultDeviceID;
         }
         private void HSMS_Load()
         {
-            XmlNode Parameters = document.SelectSingleNode("Parameters");
-            XmlNode SECS = Parameters.SelectSingleNode("SECS");
-            XmlNode HSMS = SECS.SelectSingleNode("HSMS");
+            T3_numericUpDown.Value = Parameters.HSMS_T3;
 
-            XmlElement T3 = (XmlElement)HSMS.SelectSingleNode("T3");
-            T3_numericUpDown.Value = long.Parse(T3.InnerText);
+            T5_numericUpDown.Value = Parameters.HSMS_T5;
 
-            XmlElement T5 = (XmlElement)HSMS.SelectSingleNode("T5");
-            T5_numericUpDown.Value = long.Parse(T5.InnerText);
+            T6_numericUpDown.Value = Parameters.HSMS_T6;
 
-            XmlElement T6 = (XmlElement)HSMS.SelectSingleNode("T6");
-            T6_numericUpDown.Value = long.Parse(T6.InnerText);
+            T7_numericUpDown.Value = Parameters.HSMS_T7;
 
-            XmlElement T7 = (XmlElement)HSMS.SelectSingleNode("T7");
-            T7_numericUpDown.Value = long.Parse(T7.InnerText);
+            T8_numericUpDown.Value = Parameters.HSMS_T8;
 
-            XmlElement T8 = (XmlElement)HSMS.SelectSingleNode("T8");
-            T8_numericUpDown.Value = long.Parse(T8.InnerText);
+            ConnectionMode_CB.SelectedItem = Parameters.HSMS_ConnectionMode;
 
-            ConnectionMode_CB.Items.Add(HSMS_CONNECTION_MODE.ACTIVE);
-            ConnectionMode_CB.Items.Add(HSMS_CONNECTION_MODE.PASSIVE);
-            ConnectionMode_CB.Items.Add(HSMS_CONNECTION_MODE.ALTERNATING);
-            XmlElement ConnectionMode = (XmlElement)HSMS.SelectSingleNode("ConnectionMode");
-            ConnectionMode_CB.SelectedItem = (HSMS_CONNECTION_MODE)Enum.Parse(typeof(HSMS_CONNECTION_MODE), ConnectionMode.InnerText);
+            LocalIPAddress_textBox.Text = Parameters.HSMS_LocalIPAddress;
 
-            XmlElement LocalIPAddress = (XmlElement)HSMS.SelectSingleNode("LocalIPAddress");
-            LocalIPAddress_textBox.Text = LocalIPAddress.InnerText;
+            RemoteIPAddress_textBox.Text = Parameters.HSMS_RemoteIPAddress;
 
-            XmlElement RemoteIPAddress = (XmlElement)HSMS.SelectSingleNode("RemoteIPAddress");
-            RemoteIPAddress_textBox.Text = RemoteIPAddress.InnerText;
+            LocalIPPort_numericUpDown.Value = Parameters.HSMS_LocalIPPort;
 
-            XmlElement LocalIPPort = (XmlElement)HSMS.SelectSingleNode("LocalIPPort");
-            LocalIPPort_numericUpDown.Value = uint.Parse(LocalIPPort.InnerText);
+            RemoteIPPort_numericUpDown.Value = Parameters.HSMS_RemoteIPPort;
 
-            XmlElement RemoteIPPort = (XmlElement)HSMS.SelectSingleNode("RemoteIPPort");
-            RemoteIPPort_numericUpDown.Value = uint.Parse(RemoteIPPort.InnerText);
-
-            XmlElement LinkTestTimer = (XmlElement)HSMS.SelectSingleNode("LinkTestTimer");
-            LinkTestTimer_numericUpDown.Value = uint.Parse(LinkTestTimer.InnerText);
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
+            LinkTestTimer_numericUpDown.Value = Parameters.HSMS_LinkTestTimer;
 
         }
     }
