fbd82dd8d351e04f0f6ed7a97925141764953266
AdamXu <xsz1993@live.com>
2022-01-05 00:51:06 +0800

    20220105-1251

---
+++ NET60/SystemDataSet/SystemDataSet/Class/_Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SystemDataSet
+{
+    public class _Data
+    {
+        public DataSet dataset;
+
+        public _Data (string dsName)
+        {
+            dataset = new DataSet (dsName);
+            DataTable dt = new DataTable ("Table1");
+            dt.Columns.Add("Columns1", typeof(int));
+            dt.Columns.Add("Columns2", typeof(int));
+            dt.Columns.Add("Columns3", typeof(int));
+            dt.Columns.Add("Columns4", typeof(int));
+            dt.Columns.Add("Columns5", typeof(int));
+            for(int i = 0; i < 5; i++)
+            {
+                DataRow dr = dt.NewRow ();
+                for(int j = 0; j < dr.ItemArray.Length; j++)
+                {
+                    dr[j] = i * 100 + j;
+                }
+                dt.Rows.Add(dr);
+            }
+            dataset.Tables.Add (dt);
+        }
+
+        public string GetString()
+        {
+            return dataset.DataSetName;
+        }
+        public object[] GetDataTable()
+        {
+            string[] str = new string[dataset.Tables.Count];
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = dataset.Tables[i].TableName;
+            }
+            return str;
+        }
+    }
+}
---
+++ NET60/SystemDataSet/SystemDataSet/Form1.cs
@@ -0,0 +1,39 @@
+namespace SystemDataSet
+{
+    public partial class Form1 : Form
+    {
+        public _Data _data = new _Data("Test_Data");
+        public Form1()
+        {
+            InitializeComponent();
+            DataSet_TextBox.Text = _data.GetString();
+            DataTable_ComboBox.Items.AddRange(_data.GetDataTable());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WriteXml_button_Click(object sender, EventArgs e)
+        {
+            _data.dataset.WriteXml(DataSet_TextBox.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DataTable_ComboBox.SelectedIndex >= 0)
+            {
+                _data.dataset.Tables[DataTable_ComboBox.SelectedIndex].WriteXml(DataTable_ComboBox.SelectedItem.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DataTable_ComboBox.SelectedIndex >= 0)
+            {
+                dataGridView1.DataSource = _data.dataset.Tables[DataTable_ComboBox.SelectedIndex];
+            }
+        }
+    }
+}
